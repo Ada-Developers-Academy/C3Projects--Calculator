558c8dfbe5bc1d57008037793d2f0893dbb9c044
amirahaile <amira.dhaile@gmail.com>
2015-05-17 16:56:21 -0700

    Lunch

--- calculator.rb
+++ calculator.rb
@@ -1,24 +1,58 @@
+# checks if user input is an operation
+def is_operator?(user_input)
+  operators = ["+", "add", "addition", "plus",
+               "-", "sub", "subtract", "subtraction",
+               "*", "multiply", "times",
+               "/", "divide", "division",
+               "^", "**", "power", "power of", "exponent", "exponents", "raise", "raised",
+               "%", "modulo", "modulus", "remainder"]
+
+  if user_input == ""
+    puts "Um… you gotta enter *something*, bud. Try again.\n "
+    abort
+  elsif operators.include?(user_input) == false
+    puts "Hey, now. We don't do that kind of math here.\n "
+    abort
+  else
+    return user_input
+  end
+end
+
+# checks if user input is a number
+def is_number?(user_input)
+  num = Integer(user_input) rescue false
+  if  num == false
+    puts "Hey! That's NaN! >:("
+    abort
+  else
+    return user_input.to_i
+  end
+end
+
+# accepts non-operations
 puts "Hello!\nWhat operation would you like to perform?"
-operation = gets.chomp
-# accepts non-Integers
+operation = is_operator?(gets.chomp)
 puts "Great. What's the first number you'd like to use?"
-num1 = gets.chomp.to_i
-# accepts non-Integers
+num1 = is_number?(gets.chomp)
 puts "Awesome! And the second?"
-num2 = gets.chomp.to_i
+num2 = is_number?(gets.chomp)
 
 # computes the equation
 case operation
-when "+", "add", "addition"
+when "+", "add", "addition", "plus"
+  operation_sym = "+"
   answer = num1 + num2
 
 when "-", "sub", "subtract", "subtraction"
+  operation_sym = "-"
   answer = num1 - num2
 
 when "*", "multiply", "times"
+  operation_sym = "*"
   answer = num1 * num2
 
 when "/", "divide", "division"
+  operation_sym = "/"
   if num2 == 0
     answer = "nothing! You can't divide by 0, silly"
   else
@@ -31,8 +65,19 @@ when "/", "divide", "division"
     end
   end
 
+when "^", "**", "power", "power of", "exponent", "exponents", "raise", "raised"
+  operation_sym = "^"
+  answer = num1 ** num2
+
+when "%", "modulo", "modulus", "remainder"
+  operation_sym = "%"
+  answer = num1 % num2
+
 else
+  # Should terminate when incorrect operation is submitted
+  # Check that this `abort` works
   puts "Oops, that's not an operation. Try again."
+  abort
 end
 
-puts "The answer is #{answer}."
+puts "\n#{num1} #{operation_sym} #{num2} = #{answer}"
