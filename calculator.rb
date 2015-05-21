e2f095627a80bd29e1dc5f3e20be378cfbce9ead
Loraine <loraine@lorainekv.com>
2015-05-20 23:08:10 -0700

    Updated this code per khjrtbrg's suggestions.

--- calculator.rb
+++ calculator.rb
@@ -5,37 +5,46 @@
 #Collects first number
 puts "You find a calculator on the ground. You pick it up. The screen says:\n
 Enter a number>>"
-first_num = gets.chomp.to_f
+first_num = gets.chomp
 
 #Checks first number
-while first_num != "0" && first_num.to_i == 0
+# This while loop catches text input
+while first_num.to_i == 0
+  #However, if the input is a string of zero, it allows that through.
+  if first_num.to_s == "0"
+    break
+  end
   puts "That is totally not a number! Try agin!"
-  first_num = gets.chomp.to_f
+  first_num = gets.chomp
 end
+  first_num = first_num.to_i
 
 #Collects math operation
-op_examples = "\nOPTIONS\n+, add, plus\n-, subtract, minus\n*, multiply, times\n/, divide, division\nexponent, to the power of\nmodulo"
-puts "\nNow choose a math operator! #{op_examples}"
+operator_examples = "\nOPTIONS\n+, add, plus\n-, subtract, minus\n*, multiply, times\n/, divide, division\nexponent, to the power of\nmodulo"
+puts "\nNow choose a math operator! #{operator_examples}"
 operation = gets.chomp
 
 #Checks validity of operation answer
-correct_op_answers = ["+", "add", "plus", "-", "subtract", "minus", "*", "multiply", "times", "/", "divide", "division", "exponent", "to the power of", "modulo"]
-while correct_op_answers.include?(operation) == false
-  puts op_examples
+correct_operator_options = ["+", "add", "plus", "-", "subtract", "minus", "*", "multiply", "times", "/", "divide", "division", "exponent", "to the power of", "modulo"]
+while correct_operator_options.include?(operation) == false
+  puts operator_examples
   puts "That doesn't work! Try typing one of the operations above press \'RETURN\'"
   operation = gets.chomp
 end
 
 #Collects second number
 puts "Perfect! Now enter a second number."
-second_num = gets.chomp.to_f
+second_num = gets.chomp.to_i
 
 #Checks second number
-while second_num != "0" && second_num.to_i == 0
+while second_num.to_i == 0
+  if first_num.to_s == "0"
+    break
+  end
   puts "That is totally not a number! Try agin!"
-  second_num = gets.chomp.to_f
+  second_num = gets.chomp
 end
-
+  second_num = second_num.to_i
 #_________
 #Behind-the-scenes math operations
 
@@ -66,18 +75,18 @@ elsif (operation == "divide") || (operation == "division") || (operation == "/")
   if remainder == 0
     puts "#{first_num} / #{second_num} = #{quotient}"
     puts "Your answer is #{quotient}"
-  else remainder != 0
+  else
     puts "#{first_num} / #{second_num} = #{quotient} R#{remainder}"
     puts "Your answer is #{quotient} with a remainder of #{remainder}"
   end
 
-#EXPONENTS
+#Exponents
 elsif (operation == "exponent") || (operation == "to the power of")
   exp_product = first_num ** second_num
   puts "#{first_num} ** #{second_num} = #{exp_product}"
   puts "Your answer is: #{exp_product}"
 
-#MODELO
+#Modelo
 elsif (operation == "modulo")
   mod_result = first_num % second_num
   puts "#{first_num} % #{second_num} = #{mod_result}"
