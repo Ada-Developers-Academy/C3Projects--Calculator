8fa150c5dbb622ee6f2340fc9e9ae4152f2ffd1f
Lila Garcia <lilagrc@gmail.com>
2015-05-20 17:17:22 -0700

    Updated calcultor after feedback tips

--- calculator_gold_plat.rb
+++ calculator_gold_plat.rb
@@ -6,7 +6,7 @@ puts "Welcome to this super-fun calculator. What kind of operation would you lik
 operation = $stdin.gets.chomp.downcase
 
 # Check to make sure operation isn't nil
-while operation.empty? 
+while operation.empty?
 	puts "Hmmm. You didn't enter anything. What kind of math operation would you like to do?"
 	operation = $stdin.gets.chomp.downcase
 end
@@ -18,39 +18,32 @@ num_type = $stdin.gets.chomp
 # Get user input for first number and check if first number is valid
 puts "I'll need two numbers. What is the first number?"
 first_num = $stdin.gets.chomp
-if first_num.to_i.to_s == first_num || first_num.to_f.to_s == first_num
-elsif 
+unless first_num.to_i.to_s == first_num || first_num.to_f.to_s == first_num #changed from "if" to "unless", which in my understanding is the same as "if !=" Let me know if this seems like a bad use of "unless"!
 	while first_num.to_i.to_s != first_num
 		puts "Looks like you didn't enter a number. Number, please!"
 		first_num = $stdin.gets.chomp
 	end
 end
 
-# Make first number float or integer, default is integer
-if num_type == "float"
-	first_num = first_num.to_f
-else 
-	first_num = first_num.to_i
-end
-
 # Get user input for second number and check if valid
-
 puts "What is the second number?"
 second_num = $stdin.gets.chomp
-if second_num.to_i.to_s == second_num || second_num.to_f.to_s == second_num
-elsif	
-	while second_num.to_i.to_s != second_num 
+unless second_num.to_i.to_s == second_num || second_num.to_f.to_s == second_num
+	while second_num.to_i.to_s != second_num
 		puts "Nope! Please enter a number"
 		second_num = $stdin.gets.chomp
 	end
 end
 
-# Make second number float or integer
+# Make first and second number float or integer depending on user input, default is integer
 if num_type == "float"
+	first_num = first_num.to_f
 	second_num = second_num.to_f
-else 
+else
+	first_num = first_num.to_i
 	second_num = second_num.to_i
 end
+
 
 # Determine which operation to run, and runs it
 if operation == "add" || operation == "addition" || operation == "+"
@@ -70,20 +63,20 @@ elsif operation == "exponent" || operation == "^" || operation =="exponential"
 	second_num = second_num.to_i
 	print "#{first_num}^#{second_num} = "
 	result = first_num ** second_num
-	(second_num - 1).times do 
+	(second_num - 1).times do
 	print first_num, " * "
 	end
 	if num_type == "float"
 	puts "#{first_num} = #{result.to_f}"
-	else 
+	else
 	puts "#{first_num} = #{result}"
 	end
 elsif operation == "modulo" || operation == "%"
 	result = first_num % second_num
 	puts "#{first_num} % #{second_num} = #{result}"
-else 
+else
 	puts "Oops, you chose an invalid operator. Goodbye!"
 end
-	
+
 
 
