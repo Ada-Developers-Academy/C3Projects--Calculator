fd311bf9607cfffce47abd9270f6c2950bde2fce
Katie Atrops <katrops@gmail.com>
2015-05-22 12:08:54 -0700

    made changes from code review

--- calc.rb
+++ calc.rb
@@ -1,48 +1,50 @@
 answer = nil
 puts "Hi there"
 while answer == nil 
-  puts "Please enter one of the following operations, addition, subtraction, \nmultiplication, division, exponent, or modulo:"
-  operation = gets.chomp.downcase
 #gets user input
   puts "Please enter the first number:"
-  user_num1 = gets.chomp
-  num1 = user_num1.to_f
-  
+  num1 = gets.chomp
   puts "Please enter the second number:"
-  user_num2 = gets.chomp
-  num2 = user_num2.to_f
-#test to see if user input is acutally a number
-  if user_num1.to_i == 0 && user_num1 != "0"
-    puts user_num1.to_i
-    puts "You did not enter a number. You entered #{user_num1.class} or #{user_num2.class} type."
-    next
-  elsif user_num2.to_i == 0 && user_num2 != "0"
-    puts "You did not enter a number. You entered #{user_num1.class} or #{user_num2.class} type."
-    next
+  num2 = gets.chomp
+  
 
-  elsif user_num1 == "0" 
+#test to see if user input is acutally a number
+  if num1.to_i == 0 && num1 != "0" || num2.to_i == 0 && num2 != "0"
+    puts "You did not enter a number. You entered #{num1.class} or #{num2.class} type."
+    next
+  elsif num1 == "0" 
       num1 = 0
-  elsif user_num2 == "0"
+  elsif num2 == "0"
       num2 = 0
   end
+num1 = num1.to_f
+num2 = num2.to_f
 
+puts "Please enter one of the following operations, addition, subtraction, \nmultiplication, division, exponent, or modulo:"
+operation = gets.chomp.downcase
 #tests which operation the user inputed.
   if operation == "+" || operation == "add" || operation == "addition"
+    operation = "+"
     answer = num1 + num2
     
   elsif operation == "-" || operation == "subtraction" || operation == "sub"
+    operation = "-"
     answer = num1 - num2
     
   elsif operation == "*" || operation == "multiplication"
+    operation = "*"
     answer = num1 * num2
     
   elsif operation == "/" || operation == "division"
+    operation = "/"
     answer = num1 / num2
 
   elsif operation == "**" || operation == "exponent" || operation == "^"
+    operation = "^"
     answer = num1 ** num2
 
   elsif operation == "%" || operation == "mod" || operation == "modulo"
+    operation = "%"
     answer = num1 % num2
     
   else
@@ -50,13 +52,15 @@ while answer == nil
     next
   end
   #Changes answer to a float if necessary. 
-  if answer.to_f % 1 == 0
+   # answer.to_f % 1 == 0
+  if puts answer % 1 == 0
     
     num1 = num1.to_i
     num2 = num2.to_i
     answer = answer.to_i
   end
-    puts "The result is #{num1.to_s + " " + operation + " " + num2.to_s + " = " + answer.to_s} "
+  
+    puts "The result is #{num1} #{operation} #{num2} = #{answer} "
 
 end
 
