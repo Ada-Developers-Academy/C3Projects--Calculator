2b43e69c5432a7bb1cf185d3ef6e2a12933986f1
Loren Wang <wang131.131@gmail.com>
2015-05-28 12:20:36 -0700

    changes/revisions from TA comments

--- calculator_gem.rb
+++ calculator_gem.rb
@@ -1,19 +1,21 @@
 require "colorize"
 
 puts "Let us do some simple maths! What kind of math do you want to do?"
-operation = gets.chomp
+operation_input = gets.chomp
 
 # Checks for acceptable operators.
-correct_op_answers = ["+", "add", "-", "subtract", "*", "multiply", "/", "divide", "**", "exponent", "^", "%", "modulus"]
+available_operations = ["+", "add", "-", "subtract", "*", "multiply", "/", "divide", "**", "exponent", "^", "%", "modulus"]
 
-correct_op_answers_colored = ["+".colorize(:blue), "add".colorize(:blue), "-".colorize(:cyan), "subtract".colorize(:cyan), "*".colorize(:yellow), "multiply".colorize(:yellow), "/".colorize(:green), "divide".colorize(:green), \
+# I had to create a duplicate array with the colorized operators. Otherwise, when it wouldn't recognize valid operator inputs after I h
+
+available_operations_colored = ["+".colorize(:blue), "add".colorize(:blue), "-".colorize(:cyan), "subtract".colorize(:cyan), "*".colorize(:yellow), "multiply".colorize(:yellow), "/".colorize(:green), "divide".colorize(:green), \
 "**".colorize(:magenta), "exponent".colorize(:magenta), "^".colorize(:magenta), "%".colorize(:red), "modulus".colorize(:red)]
 
 # Used a while loop to display the same error message until a valid operator is entered.
-while correct_op_answers.include?(operation) == false
-  puts correct_op_answers_colored
-  puts "That ain\'t right. Use one of the coloured options above.".colorize(:black).colorize( :background => :white)
-  operation = gets.chomp
+while available_operations.include?(operation_input) == false
+  puts available_operations_colored
+  puts "That ain't right. Use one of the coloured options above.".colorize(:black).colorize( :background => :white)
+  operation_input = gets.chomp
 end
 
 # asks the user for the first number
@@ -26,22 +28,17 @@ second_number = gets.chomp.to_i
 
 # if the user enters one of the valid operators, the calculator will use one of the
 # corresponding interpolations below to output the result
-if operation == "add" || operation == "+"
-  add_result = first_number + second_number
-  puts "#{first_number} + #{second_number} = " + "  #{add_result}  ".colorize(:blue).colorize(:background => :cyan)
-elsif operation == "subtract" || operation == "-"
-  subtract_result = first_number - second_number
-  puts "#{first_number} - #{second_number} = " + "  #{subtract_result}  ".colorize(:blue).colorize(:background => :cyan)
-elsif operation == "multiply" || operation == "*"
-  multiply_result = first_number * second_number
-  puts "#{first_number} * #{second_number} = " + "  #{multiply_result}  ".colorize(:blue).colorize(:background => :cyan)
-elsif operation == "divide" || operation == "/"
-  divide_result = first_number / second_number
-  puts "#{first_number} / #{second_number} = " + "  #{divide_result}  ".colorize(:blue).colorize(:background => :cyan)
-elsif operation == "modulus" || operation == "%"
-  modulus_result = first_number % second_number
-  puts "#{first_number} % #{second_number} = " + "  #{modulus_result}  ".colorize(:blue).colorize(:background => :cyan)
-elsif operation == "exponent" || operation == "**"|| operation == "^"
-  exponent_result = first_number ** second_number
-  puts "#{first_number} ** #{second_number} = " + "  #{exponent_result}  ".colorize(:blue).colorize(:background => :cyan)
+if operation_input == "add" || operation_input == "+"
+  result = first_number + second_number
+elsif operation_input == "subtract" || operation_input == "-"
+  result = first_number - second_number
+elsif operation_input == "multiply" || operation_input == "*"
+  result = first_number * second_number
+elsif operation_input == "divide" || operation_input == "/"
+  result = first_number / second_number
+elsif operation_input == "modulus" || operation_input == "%"
+  result = first_number % second_number
+elsif operation_input == "exponent" || operation_input == "**"|| operation_input == "^"
+  result = first_number ** second_number
 end
+  puts "#{first_number} #{operation_input} #{second_number} = #{result}".colorize(:blue).colorize(:background => :cyan)
